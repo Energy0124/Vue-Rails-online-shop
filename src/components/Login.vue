76b90dbbb482162afee229e0092e25b60e9e16d2
Energy <energystar.0124@gmail.com>
2017-11-19 09:38:34 +0800

    add login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <v-breadcrumbs divider="/">
+      <v-breadcrumbs-item
+        v-for="item in breadcrumbs" :key="item.text"
+        :disabled="item.disabled" :to="item.to" :exact=true
+      >
+        {{ item.text }}
+      </v-breadcrumbs-item>
+    </v-breadcrumbs>
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data () {
+      return {
+        msg: 'Login',
+        breadcrumbs: [
+          {
+            text: 'Login',
+            disabled: false,
+            to: '/'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
